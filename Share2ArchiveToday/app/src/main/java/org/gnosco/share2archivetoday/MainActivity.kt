cbdaf02187156db0c5d8354de1004cfc409d80f9
Gabriel Fair <2096785+gabefair@users.noreply.github.com>
2024-06-19 10:48:52 -0400

    First Blood
    Not sure what I'm doing is the correct or best way

---
+++ Share2ArchiveToday/app/src/main/java/org/gnosco/share2archivetoday/MainActivity.kt
@@ -0,0 +1,60 @@
+package org.gnosco.share2archivetoday
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import org.gnosco.share2archivetoday.ui.theme.Share2ArchiveTodayTheme
+
+import android.net.Uri
+import android.util.Log
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        handleShareIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        handleShareIntent(intent)
+    }
+
+    private fun handleShareIntent(intent: Intent?) {
+        if (intent?.action == Intent.ACTION_SEND && intent.type == "text/plain") {
+            intent.getStringExtra(Intent.EXTRA_TEXT)?.let { sharedText ->
+                Log.d("MainActivity", "Shared text: $sharedText")
+                openInBrowser("https://archive.is/?run=1&url=$sharedText")
+            }
+        }
+    }
+
+    private fun openInBrowser(url: String) {
+        Log.d("MainActivity", "Opening URL: $url")
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(browserIntent)
+        finish()
+    }
+
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Share2ArchiveTodayTheme {
+        Greeting("Android")
+    }
+}
