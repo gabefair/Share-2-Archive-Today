fe858140b9a6ddce7da468cd72e63ce240ad3c9c
Gabriel Fair <2096785+gabefair@users.noreply.github.com>
2024-07-23 16:51:16 -0400

    Remove si trackers from youtube links

--- Share2ArchiveToday/app/src/main/java/org/gnosco/share2archivetoday/MainActivity.kt
+++ Share2ArchiveToday/app/src/main/java/org/gnosco/share2archivetoday/MainActivity.kt
@@ -31,13 +31,44 @@ class MainActivity : ComponentActivity() {
                 Log.d("MainActivity", "Shared text: $sharedText")
                 val url = extractUrl(sharedText)
                 if (url != null) {
-                    openInBrowser("https://archive.is/?run=1&url=${Uri.encode(url)}")
+                    val cleanedUrl = cleanYoutubeUrl(url)
+                    openInBrowser("https://archive.is/?run=1&url=${Uri.encode(cleanedUrl)}")
                 }
             }
 
         }
         finish()
     }
+
+    private fun cleanYoutubeUrl(url: String): String {
+        val uri = Uri.parse(url)
+        if (uri.host?.contains("youtube.com") == true || uri.host?.contains("youtu.be") == true) {
+            val newUriBuilder = uri.buildUpon().clearQuery()
+
+            uri.queryParameterNames.forEach { param ->
+                if (param != "si") {
+                    newUriBuilder.appendQueryParameter(param, uri.getQueryParameter(param))
+                }
+            }
+
+            // Special handling for nested query parameters like `redir_token`
+            val nestedQueryParams = uri.getQueryParameter("q")
+            if (nestedQueryParams != null) {
+                val nestedUri = Uri.parse(nestedQueryParams)
+                val newNestedUriBuilder = nestedUri.buildUpon().clearQuery()
+
+                nestedUri.queryParameterNames.forEach { nestedParam ->
+                    newNestedUriBuilder.appendQueryParameter(nestedParam, nestedUri.getQueryParameter(nestedParam))
+                }
+
+                newUriBuilder.appendQueryParameter("q", newNestedUriBuilder.build().toString())
+            }
+
+            return newUriBuilder.build().toString()
+        }
+        return url
+    }
+
 
     private fun extractUrl(text: String): String? {
         val matcher = PatternsCompat.WEB_URL.matcher(text)
